c3f055005ab84544faf57cb8619186ce6fbd8183
Mateusz Miotk <mateusz.miotk@ug.edu.pl>
2022-02-09 10:05:17 +0100

    Add some information in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import personal from "./data/personal.json";
+import mail from "./images/mail.png"
 function App() {
   return (
     <div className="App">
       <h1>Mateusz Miotk homepage in React</h1>
+        <h2>Kontakt oraz konsultacje</h2>
+        <p>
+            <img alt="" src={mail}/><br/>
+                <strong>Pokój</strong>: <mark><strong>4.10</strong></mark> (nowy budynek Instytutu Informatyki)<br/>
+                <strong>Konsultacje w semestrze zimowym: <mark> {personal.day_of_consultation} godz. {personal.hour_of_consultation} - Pokój 4.10</mark></strong><br/>
+                <strong>Istnieje możliwość komunikowania się ze mną za pomocą MS Teams prawie 24/7.</strong><br/>
+
+                <strong><mark>Zajęcia są prowadzone przez platformę MSTeams, <a href="https://stepik.org">stepik.org</a> oraz <a href="https://katacoda.com">katacoda.com</a>.</mark></strong>
+        </p>
     </div>
   );
 }
